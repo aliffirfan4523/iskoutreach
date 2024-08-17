52f6bda5c18d913eaccb1184f7d2622ac93566b1
aliff irfan <aliffirfanisk@gmail.com>
2024-08-18 02:15:43 +0800

    add new picture to homepage image list

--- lib/src/view/Homepage/home_page_image.dart
+++ lib/src/view/Homepage/home_page_image.dart
@@ -8,54 +8,50 @@ class HomePageImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final List<String> homepageimglist = [
+      'assets/images/homepage/1.jpg',
+      'assets/images/homepage/2.jpg',
+      'assets/images/homepage/3.jpg',
+      'assets/images/homepage/4.jpeg',
+      'assets/images/homepage/5.jpg',
+      'assets/images/homepage/6.jpg',
+      'assets/images/homepage/7.jpg',
+      'assets/images/homepage/8.jpeg',
+      'assets/images/homepage/9.jpeg',
+      'assets/images/homepage/10.jpeg',
+      'assets/images/homepage/11.jpeg',
+      //'assets/images/homepage/12.jpg',
+      'assets/images/homepage/13.jpg',
+    ];
 
-  	final List<String> homepageimglist = [
-		  'assets/images/homepage/1.jpg',
-		  'assets/images/homepage/2.jpg',
-		  'assets/images/homepage/3.jpg',
-		  'assets/images/homepage/4.jpeg',
-		  'assets/images/homepage/5.jpg',
-		  'assets/images/homepage/6.jpg',
-		  'assets/images/homepage/7.jpg',
-		  'assets/images/homepage/8.jpeg',
-		  'assets/images/homepage/9.jpeg',
-		  'assets/images/homepage/10.jpeg',
-		  'assets/images/homepage/11.jpeg',
-		  //'assets/images/homepage/12.jpg',
-		];
+    final List<Widget> homepageimageslider = homepageimglist
+        .map((item) => Container(
+              child: Container(
+                margin: const EdgeInsets.all(5.0),
+                child: ClipRRect(
+                    borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+                    child: Stack(
+                      children: <Widget>[
+                        Center(
+                            child: Container(
+                                decoration: BoxDecoration(
+                          image: DecorationImage(
+                              image: AssetImage(item), fit: BoxFit.fitWidth),
+                        ))),
+                      ],
+                    )),
+              ),
+            ))
+        .toList();
 
-		final List<Widget> homepageimageslider = homepageimglist.map(
-		  (item) => Container(
-		    child: Container(
-		      margin: const EdgeInsets.all(5.0),
-		      child: ClipRRect(
-		        borderRadius: const BorderRadius.all(Radius.circular(5.0)),
-		        child: Stack(
-		          children: <Widget>[
-		            Center(
-		              child: Container(
-		                decoration: BoxDecoration(
-		                  image: DecorationImage(
-		                    image: AssetImage(item), fit: BoxFit.fitWidth
-		                  ),
-		                )
-		              )
-		            ),
-		          ],
-		        )
-		      ),
-		    ),
-		  )
-		).toList();
-
-  	return Swiper(
-  		loop: true,
-  		itemCount: homepageimglist.length,
-  		itemBuilder: (BuildContext context, int index) => Image.asset(
-  			homepageimglist[index],
-  			fit: BoxFit.cover,
-  		),
-  		autoplay: true,
-  	);
+    return Swiper(
+      loop: true,
+      itemCount: homepageimglist.length,
+      itemBuilder: (BuildContext context, int index) => Image.asset(
+        homepageimglist[index],
+        fit: BoxFit.cover,
+      ),
+      autoplay: true,
+    );
   }
 }
